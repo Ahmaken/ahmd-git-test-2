47e87bf102d0a1dffa358338b7f7d8610fe0981a
ahmadanikennunimad <alkafdami@gmail.com>
2024-11-21 10:25:30 +0700

    nambahaken list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Assalaam Alaykum</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia rem consequatur vitae suscipit iure ipsam. Sed dignissimos alias ratione eius deserunt, cupiditate veniam vero illo optio earum delectus officiis ea?</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
